0555ab766efa4ef0cee3ee24e22ab0d243c14a06
chandrasekharsurapaneni <chandusurapaneni30@gmail.com>
2023-09-02 10:13:26 +0530

    git ignore

--- app.js
+++ app.js
@@ -117,8 +117,8 @@ res.redirect("/");
 
 
 
+const port = process.env.PORT || 3000
 
-
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(port, function() {
+  console.log(`server started to listen at ${port}`);
 });
